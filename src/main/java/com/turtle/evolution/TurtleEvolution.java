00b4f3bdf55c6ac1b9d9096eeb01b3ab14f8e6ae
zazaDestroyer <akynow.azamat@gmail.com>
2026-05-28 16:12:54 +0600

    Added the effect when player is wearing the turtle helmet

--- src/main/java/com/turtle/evolution/TurtleEvolution.java
+++ src/main/java/com/turtle/evolution/TurtleEvolution.java
@@ -1,7 +1,11 @@
 package com.turtle.evolution;
 
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
@@ -9,10 +13,15 @@ import org.apache.logging.log4j.core.jmx.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+
+
 public class TurtleEvolution implements ModInitializer {
 	public static final String MOD_ID = "turtle-evolution";
-
-
+	private static final Set<UUID> turtleHelmetPlayers = new HashSet<>();
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
@@ -20,10 +29,17 @@ public class TurtleEvolution implements ModInitializer {
 		ServerTickEvents.END_SERVER_TICK.register(server -> {
 			server.getPlayerList().getPlayers().forEach(player -> {
 				var helmet = player.getItemBySlot(EquipmentSlot.HEAD);
-				
-				if (helmet.getItem().equals(Items.TURTLE_HELMET)) {
-					LOGGER.info(player.getName().getString());
+
+				if (helmet.getItem() == Items.TURTLE_HELMET) {
+					player.addEffect(
+							new MobEffectInstance(
+									MobEffects.NIGHT_VISION,
+									10000,
+									2
+							)
+					);
 				}
+				
 			});
 		});
 	}
